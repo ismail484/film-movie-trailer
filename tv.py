3147d66b84d3a02bb6b3a3e86299a154606b4f67
Ismail <Mohamed.Ahmed.Ismail@dnvgl.com>
2017-05-06 13:19:43 +0200

    executable files

--- tv.py
+++ tv.py
@@ -5,11 +5,12 @@ class TvShow(videos.Video):
     
 
 
-    def __init__(self,title,duration,season,tv_station,episode_url):
+    def __init__(self,title,duration,season,tv_station,poster_image_url,episode_url):
         
         videos.Video.__init__(self,title,duration)
         self.season= season
         self.tv_station=tv_station
+        self.poster_image_url=poster_image_url
         self.episode_url=episode_url
 
     def get_local_listing(self):
--- tv_center.py
+++ tv_center.py
@@ -1,44 +1,44 @@
-import website
+import website_tv
 import tv
 import videos
 
-toy_story= media.Movie("Toy Story",5,"a story of a boy and his toys",
+toy_story= tv.TvShow("Toy Story",5,"this is season1", "this is  channel 1",
                      "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                      "https://www.youtube.com/watch?v=4KPTXpQehio")
 #print(toy_story.storyline)
 
 
-avatar= media.Movie("Avatar",5,"a story of a boy and his toys",
+avatar= tv.TvShow("Toy Story",5,"this is season1", "this is  channel 1",
                      "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                      "https://www.youtube.com/watch?v=4KPTXpQehio")
 
 #avatar.show_trailer()
 
 
-School_of_rock= media.Movie("school of rock",5,"a story of a boy and his toys",
+School_of_rock= tv.TvShow("Toy Story",5,"this is season1", "this is  channel 1",
                      "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                      "https://www.youtube.com/watch?v=4KPTXpQehio")
 
-home_alone= media.Movie("home_alone",5,"a story of a boy and his toys",
+home_alone= tv.TvShow("Toy Story",5,"this is season1", "this is  channel 1",
                      "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                      "https://www.youtube.com/watch?v=4KPTXpQehio")
 
 
-the_rock= media.Movie("the rock",5,"a story of a boy and his toys",
+the_rock= tv.TvShow("Toy Story",5,"this is season1", "this is  channel 1",
                      "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                      "https://www.youtube.com/watch?v=4KPTXpQehio")
 
-midnight_in_paris= media.Movie("midnight_in_paris",5,"a story of a boy and his toys",
+midnight_in_paris= tv.TvShow("Toy Story",5,"this is season1", "this is  channel 1",
                      "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                      "https://www.youtube.com/watch?v=4KPTXpQehio")
 
-hunger_games= media.Movie("hunger_games",5,"a story of a boy and his toys",
+hunger_games=tv.TvShow("Toy Story",5,"this is season1", "this is  channel 1",
                      "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                      "https://www.youtube.com/watch?v=4KPTXpQehio")
 
-movies=[toy_story,avatar,School_of_rock,home_alone,the_rock,midnight_in_paris,hunger_games]
+programs=[toy_story,avatar,School_of_rock,home_alone,the_rock,midnight_in_paris,hunger_games]
 
-website.open_movies_page(movies)
+website_tv.open_movies_page(programs)
 
 #print(media.Movie.VALID_RATINGS)
 #print( media.Movie.__doc__)
