227a7eb79bdfcfd259402ffdd1a542fb5597a160
Ismail <Mohamed.Ahmed.Ismail@dnvgl.com>
2017-05-06 13:17:25 +0200

    modify posters

--- entertainment_center.py
+++ entertainment_center.py
@@ -10,32 +10,32 @@ toy_story= media.Movie("Toy Story",5,"a story of a boy and his toys",
 
 
 avatar= media.Movie("Avatar",5,"a story of a boy and his toys",
-                     "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                     "https://www.youtube.com/watch?v=4KPTXpQehio")
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 #avatar.show_trailer()
 
 
-School_of_rock= media.Movie("school of rock",5,"a story of a boy and his toys",
-                     "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                     "https://www.youtube.com/watch?v=4KPTXpQehio")
+School_of_rock= media.Movie("School of Rock",5,"a story of a boy and his toys",
+                     "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                     "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
 
-home_alone= media.Movie("home_alone",5,"a story of a boy and his toys",
-                     "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                     "https://www.youtube.com/watch?v=4KPTXpQehio")
+home_alone= media.Movie("Home alone 2",5,"a story of a boy and his toys",
+                     "https://upload.wikimedia.org/wikipedia/en/5/50/Home_Alone_2.jpg",
+                     "https://www.youtube.com/watch?v=z_An3W-oO2k")
 
 
-the_rock= media.Movie("the rock",5,"a story of a boy and his toys",
-                     "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                     "https://www.youtube.com/watch?v=4KPTXpQehio")
+the_rock= media.Movie("The Rock",5,"a story of a boy and his toys",
+                     "https://upload.wikimedia.org/wikipedia/en/8/82/The_Rock_%28movie%29.jpg",
+                     "https://www.youtube.com/watch?v=313n0wga2xo")
 
-midnight_in_paris= media.Movie("midnight_in_paris",5,"a story of a boy and his toys",
-                     "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                     "https://www.youtube.com/watch?v=4KPTXpQehio")
+midnight_in_paris= media.Movie("Midnight in Paris",5,"a story of a boy and his toys",
+                     "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                     "https://www.youtube.com/watch?v=FAfR8omt-CY")
 
-hunger_games= media.Movie("hunger_games",5,"a story of a boy and his toys",
-                     "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                     "https://www.youtube.com/watch?v=4KPTXpQehio")
+hunger_games= media.Movie("Hunger Games",5,"a story of a boy and his toys",
+                     "https://upload.wikimedia.org/wikipedia/en/4/4b/Hunger_Games_Film_Poster.jpg",
+                     "https://www.youtube.com/watch?v=mfmrPu43DF8")
 
 movies=[toy_story,avatar,School_of_rock,home_alone,the_rock,midnight_in_paris,hunger_games]
 
